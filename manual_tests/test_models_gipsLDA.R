77bb14d465ca1a7e42e1724064d3b223223e98ba
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2025-11-21 23:31:08 +0100

    initial manual tests gipsLDA and gipsQDA

---
+++ manual_tests/test_models_gipsLDA.R
@@ -0,0 +1,34 @@
+# setwd("repo_inz/gipsDAInzynierka")
+set.seed(42)
+
+scenario_1_df = read.csv("data/scenario_1.csv")
+train_indexes = sample(1:nrow(scenario_1_df), size = 0.7 * nrow(scenario_1_df))
+train_data = scenario_1_df[train_indexes, ]
+test_data = scenario_1_df[-train_indexes, ]
+
+model_gipsLDA = lda(Y~., data = train_data)
+print(model_gipsLDA)
+plot(model_gipsLDA)
+
+predictions_gipsLDA = predict(model_gipsLDA, newdata = test_data)
+print(predictions_gipsLDA$class)
+print(predictions_gipsLDA$posterior)
+confusion_matrix_gipsLDA = table(test_data$Y, predictions_gipsLDA$class)
+print(confusion_matrix_gipsLDA)
+accuracy_gipsLDA = sum(diag(confusion_matrix_gipsLDA)) / sum(confusion_matrix_gipsLDA)
+print(paste("Accuracy of gipsLDA:", round(accuracy_gipsLDA * 100, 2), "%"))
+
+# compare to MASS::lda
+
+model_MASS_lda = MASS::lda(Y~., data = train_data)
+print(model_MASS_lda)
+plot(model_MASS_lda)
+
+predictions_MASS_lda = predict(model_MASS_lda, newdata = test_data)
+print(predictions_MASS_lda$class)
+print(predictions_MASS_lda$posterior)
+confusion_matrix_MASS_lda = table(test_data$Y, predictions_MASS_lda$class)
+print(confusion_matrix_MASS_lda)
+accuracy_MASS_lda = sum(diag(confusion_matrix_MASS_lda)) / sum(confusion_matrix_MASS_lda)
+print(paste("Accuracy of MASS::lda:", round(accuracy_MASS_lda * 100, 2), "%"))
+
---
+++ manual_tests/test_models_gipsQDA.R
@@ -0,0 +1,29 @@
+# setwd("repo_inz/gipsDAInzynierka")
+set.seed(42)
+
+scenario_3_df = read.csv("data/scenario_3.csv")
+train_indexes = sample(1:nrow(scenario_3_df), size = 0.7 * nrow(scenario_3_df))
+train_data = scenario_3_df[train_indexes, ]
+test_data = scenario_3_df[-train_indexes, ]
+
+model_gipsQDA = qda(Y~., data = train_data)
+
+predictions_gipsQDA = predict(model_gipsQDA, newdata = test_data)
+print(predictions_gipsQDA$class)
+print(predictions_gipsQDA$posterior)
+confusion_matrix_gipsQDA = table(test_data$Y, predictions_gipsQDA$class)
+print(confusion_matrix_gipsQDA)
+accuracy_gipsQDA = sum(diag(confusion_matrix_gipsQDA)) / sum(confusion_matrix_gipsQDA)
+print(paste("Accuracy of gipsLDA:", round(accuracy_gipsQDA * 100, 2), "%"))
+
+# compare to MASS::qda
+
+model_MASS_qda = MASS::qda(Y~., data = train_data)
+
+predictions_MASS_qda = predict(model_MASS_qda, newdata = test_data)
+print(predictions_MASS_qda$class)
+print(predictions_MASS_qda$posterior)
+confusion_matrix_MASS_qda = table(test_data$Y, predictions_MASS_qda$class)
+print(confusion_matrix_MASS_qda)
+accuracy_MASS_qda = sum(diag(confusion_matrix_MASS_qda)) / sum(confusion_matrix_MASS_qda)
+print(paste("Accuracy of MASS::qda:", round(accuracy_MASS_qda * 100, 2), "%"))
