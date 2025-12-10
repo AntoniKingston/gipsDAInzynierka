4454b66ac9e8765a9d34d06fae473965c216f431
AntoniKingston <qqtrol@gmail.com>
2025-12-10 14:30:30 +0100

    poprawienie wygładzania i pozbycie się pca

--- manual_tests/models/test_models_heart.R
+++ manual_tests/models/test_models_heart.R
@@ -1,15 +1,18 @@
 set.seed(2137)
 source("manual_tests/models/utils.R")
-df <- read.csv("data/real_world_datasets/heart.csv")
+df <- read.csv("data/heart.csv")
 colnames(df)[colnames(df) == "HeartDisease"] <- "Y"
-df_encoded <- fastDummies::dummy_cols(df, select_columns = c("Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"), remove_selected_columns = TRUE)[sample(1:nrow(df)),]
+df_encoded <- fastDummies::dummy_cols(df, select_columns = c("Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"), remove_selected_columns = TRUE, remove_first_dummy = TRUE)[sample(1:nrow(df)),]
 y <- df_encoded$Y
-pca_obj <- prcomp(subset(df_encoded, select = -c(Y)), scale. = TRUE)
-pca_frame <- data.frame(pca_obj$x)
-pca_frame <- pca_frame[,1:10]
-pca_frame$Y <- y
-heart_plot_data <- generate_single_plot_info(pca_frame, granularity = 25, lb = 200,  n_experiments = 5, opt = "MH", max_iter = 100, tr_ts_split = 0.7, MAP = TRUE)
+# pca_obj <- prcomp(subset(df_encoded, select = -c(Y)), scale. = TRUE)
+# pca_frame <- data.frame(pca_obj$x)
+# pca_frame <- pca_frame[,1:10]
+# pca_frame$Y <- y
+heart_plot_data <- generate_single_plot_info(df_encoded, granularity = 25, lb = 200,  n_experiments = 5, opt = "MH", max_iter = 100, tr_ts_split = 0.7, MAP = TRUE)
 real_plot_data = list("heart" = heart_plot_data)
+
+
+
+
 create_multilevel_plot(real_plot_data)
-pca_frame
-heart_plot_data
+
--- manual_tests/models/utils.R
+++ manual_tests/models/utils.R
@@ -1,76 +1,145 @@
 library(gipsDA)
 library(ggplot2)
 
-accuracy_experiment <- function(train_data, test_data, model, MAP = TRUE, opt = "BF", max_iter = 100, tr_ts_split = 0.7) {
-  # classifier <- tryCatch({
-  #   if (model == "lda") {
-  #     MASS::lda(Y ~ ., data = train_data, method = "mve")
-  #   } else if (model == "qda") {
-  #     MASS::qda(Y ~ ., data = train_data, method = "mve")
-  #   } else if (model == "gipsldacl") {
-  #     gipslda(Y ~ ., data = train_data, weighted_avg = FALSE, MAP = MAP, optimizer = opt, max_iter = max_iter)
-  #   } else if (model == "gipsldawa") {
-  #     gipslda(Y ~ ., data = train_data, weighted_avg = TRUE, MAP = MAP, optimizer = opt, max_iter = max_iter)
-  #   } else if (model == "gipsqda") {
-  #     gipsqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
-  #   } else {
-  #     gipsmultqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
-  #   }
-  # }, error = function(e) {
-  #   warning(sprintf("Error fitting model: %s ", model))
-  #   NULL
-  # })
+generate_splits <- function(n, tr_ts_split, n_experiments) {
+  replicate(
+    n_experiments,
+    {
+      train_idx <- sample(seq_len(n), size = floor(tr_ts_split * n))
+      list(
+        train = train_idx,
+        test  = setdiff(seq_len(n), train_idx)
+      )
+    },
+    simplify = FALSE
+  )
+}
+
+accuracy_experiment <- function(df, split, model,
+                                MAP = TRUE, opt = "BF", max_iter = 100) {
+
+  train_data <- df[split$train, , drop = FALSE]
+  test_data  <- df[split$test,  , drop = FALSE]
 
   if (model == "lda") {
-    classifier <- MASS::lda(Y ~ ., data = train_data, method = "mve")
+    classifier <- MASS::lda(Y ~ ., data = train_data, method = "moment")
   } else if (model == "qda") {
-    classifier <- MASS::qda(Y ~ ., data = train_data, method = "mve")
+    classifier <- MASS::qda(Y ~ ., data = train_data, method = "moment")
   } else if (model == "gipsldacl") {
-    classifier <- gipslda(Y ~ ., data = train_data, weighted_avg = FALSE, MAP = MAP, optimizer = opt, max_iter = max_iter)
+    classifier <- gipslda(Y ~ ., data = train_data,
+                          weighted_avg = FALSE,
+                          MAP = MAP, optimizer = opt, max_iter = max_iter)
   } else if (model == "gipsldawa") {
-    classifier <- gipslda(Y ~ ., data = train_data, weighted_avg = TRUE, MAP = MAP, optimizer = opt, max_iter = max_iter)
+    classifier <- gipslda(Y ~ ., data = train_data,
+                          weighted_avg = TRUE,
+                          MAP = MAP, optimizer = opt, max_iter = max_iter)
   } else if (model == "gipsqda") {
-    classifier <- gipsqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
+    classifier <- gipsqda(Y ~ ., data = train_data,
+                          MAP = MAP, optimizer = opt, max_iter = max_iter)
   } else {
-    classifier <- gipsmultqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
-  }
-
-
-  if (is.null(classifier)) {
-    return(0)
+    classifier <- gipsmultqda(Y ~ ., data = train_data,
+                              MAP = MAP, optimizer = opt, max_iter = max_iter)
   }
 
   pred <- predict(classifier, test_data)$class
-  y_true <- test_data$Y
-  acc <- mean(pred == y_true)
-  return(acc)
+  mean(pred == test_data$Y)
 }
+
+
+# accuracy_experiment <- function(train_data, test_data, model, MAP = TRUE, opt = "BF", max_iter = 100, tr_ts_split = 0.7) {
+#   # classifier <- tryCatch({
+#   #   if (model == "lda") {
+#   #     MASS::lda(Y ~ ., data = train_data, method = "mve")
+#   #   } else if (model == "qda") {
+#   #     MASS::qda(Y ~ ., data = train_data, method = "mve")
+#   #   } else if (model == "gipsldacl") {
+#   #     gipslda(Y ~ ., data = train_data, weighted_avg = FALSE, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   #   } else if (model == "gipsldawa") {
+#   #     gipslda(Y ~ ., data = train_data, weighted_avg = TRUE, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   #   } else if (model == "gipsqda") {
+#   #     gipsqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   #   } else {
+#   #     gipsmultqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   #   }
+#   # }, error = function(e) {
+#   #   warning(sprintf("Error fitting model: %s ", model))
+#   #   NULL
+#   # })
+#
+#   if (model == "lda") {
+#     classifier <- MASS::lda(Y ~ ., data = train_data, method = "moment")
+#   } else if (model == "qda") {
+#     classifier <- MASS::qda(Y ~ ., data = train_data, method = "moment")
+#   } else if (model == "gipsldacl") {
+#     classifier <- gipslda(Y ~ ., data = train_data, weighted_avg = FALSE, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   } else if (model == "gipsldawa") {
+#     classifier <- gipslda(Y ~ ., data = train_data, weighted_avg = TRUE, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   } else if (model == "gipsqda") {
+#     classifier <- gipsqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   } else {
+#     classifier <- gipsmultqda(Y ~ ., data = train_data, MAP = MAP, optimizer = opt, max_iter = max_iter)
+#   }
+#
+#
+#   if (is.null(classifier)) {
+#     return(0)
+#   }
+#
+#   pred <- predict(classifier, test_data)$class
+#   y_true <- test_data$Y
+#   acc <- mean(pred == y_true)
+#   return(acc)
+# }
 
 generate_accuracy_data <- function(df,
                                    model,
+                                   splits_per_n,
                                    granularity = 25,
                                    lb = 50,
                                    n_experiments = 5,
                                    MAP = TRUE,
                                    opt = "BF",
-                                   max_iter = 100,
-                                   tr_ts_split = 0.7) {
-  if(nrow(df) < lb) {
-  rlang::abort(c("x" = "dataset has less rows than specified lower bound"))
+                                   max_iter = 100) {
+
+  if (nrow(df) < lb) {
+    rlang::abort("dataset has less rows than specified lower bound")
   }
 
-  ns_obs <- round(exp(seq(log(lb), log(nrow(df)), length.out = granularity)))
-  accs <- c()
-  for (n_obs in ns_obs) {
-    df_temp <- df[1:n_obs,]
-    train_indexes = sample(1:nrow(df_temp), size = tr_ts_split * nrow(df_temp))
-    train_data = df_temp[train_indexes, ]
-    test_data = df_temp[-train_indexes, ]
-    accs <- c(accs, mean(rep(accuracy_experiment(train_data, test_data, model, MAP = MAP, opt = opt, max_iter = max_iter, tr_ts_split = 0.7), n_experiments)))
-  }
+  accs <- lapply(splits_per_n, function(splits) {
+    mean(vapply(splits, function(split) {
+      accuracy_experiment(df, split, model, MAP = MAP, opt = opt, max_iter = max_iter)
+    }, numeric(1)))
+  })
 
-  return(list("accs" = accs, "ns_obs" = ns_obs))
+  return(accs)
 }
+
+
+# generate_accuracy_data <- function(df,
+#                                    model,
+#                                    granularity = 25,
+#                                    lb = 50,
+#                                    n_experiments = 5,
+#                                    MAP = TRUE,
+#                                    opt = "BF",
+#                                    max_iter = 100,
+#                                    tr_ts_split = 0.7) {
+#   if(nrow(df) < lb) {
+#   rlang::abort(c("x" = "dataset has less rows than specified lower bound"))
+#   }
+#
+#   ns_obs <- round(exp(seq(log(lb), log(nrow(df)), length.out = granularity)))
+#   accs <- c()
+#   for (n_obs in ns_obs) {
+#     df_temp <- df[1:n_obs,]
+#     train_indexes = sample(1:nrow(df_temp), size = tr_ts_split * nrow(df_temp))
+#     train_data = df_temp[train_indexes, ]
+#     test_data = df_temp[-train_indexes, ]
+#     accs <- c(accs, mean(rep(accuracy_experiment(train_data, test_data, model, MAP = MAP, opt = opt, max_iter = max_iter, tr_ts_split = 0.7), n_experiments)))
+#   }
+#
+#   return(list("accs" = accs, "ns_obs" = ns_obs))
+# }
 
 generate_single_plot_info <- function(scenario_data,
                                       model_names = c("lda", "qda", "gipsldacl", "gipsldawa", "gipsqda", "gipsmultqda"),
@@ -81,10 +150,33 @@ generate_single_plot_info <- function(scenario_data,
                                       opt = "BF",
                                       max_iter = 100,
                                       tr_ts_split = 0.7) {
-  plot_info <- lapply(model_names, function (x) generate_accuracy_data(scenario_data, x, granularity, lb, n_experiments, opt, max_iter, tr_ts_split, MAP = MAP))
+
+  ns_obs <- round(exp(seq(log(lb), log(nrow(scenario_data)), length.out = granularity)))
+
+  splits_per_n <- lapply(ns_obs, function(n_obs) {
+    generate_splits(n_obs, tr_ts_split, n_experiments)
+  })
+
+  plot_info <- lapply(model_names, function(model) {
+    list("accs" = generate_accuracy_data(scenario_data, model, splits_per_n, granularity, lb, n_experiments, MAP, opt, max_iter), "ns_obs" = ns_obs)
+  })
   names(plot_info) <- model_names
-  return(plot_info)
+  plot_info
 }
+
+# generate_single_plot_info <- function(scenario_data,
+#                                       model_names = c("lda", "qda", "gipsldacl", "gipsldawa", "gipsqda", "gipsmultqda"),
+#                                       granularity = 25,
+#                                       lb = 50,
+#                                       n_experiments = 5,
+#                                       MAP = TRUE,
+#                                       opt = "BF",
+#                                       max_iter = 100,
+#                                       tr_ts_split = 0.7) {
+#   plot_info <- lapply(model_names, function (x) generate_accuracy_data(scenario_data, x, granularity, lb, n_experiments, opt, max_iter, tr_ts_split, MAP = MAP))
+#   names(plot_info) <- model_names
+#   return(plot_info)
+# }
 
 generate_multiple_plots_info <- function(data_path,
                              scenario_names = c("lda", "qda", "gipslda", "gipsqda", "gipsmultqda"),
@@ -108,100 +200,100 @@ generate_multiple_plots_info <- function(data_path,
 ## przykładowe wywołanie (działa jak się dobrze dane wygeneruje)
 # mult_plots_info <- generate_multiple_plots_info("data", granularity = 3, n_experiments = 1)
 
-plot_multilevel <- function(
-  mult_plots_info,
-  ncol = NULL,                   # number of columns in layout
-  xlab = "Number of observations",
-  ylab = "Accuracy",
-  lwd = 2,
-  point_pch = 16,
-  legend_cex = 0.7
-) {
-  stopifnot(is.list(mult_plots_info))
-
-  outer_names <- names(mult_plots_info)
-  n_plots <- length(outer_names)
-
-  # determine layout
-  if (is.null(ncol)) {
-    ncol <- ceiling(sqrt(n_plots))
-  }
-  nrow <- ceiling(n_plots / ncol)
-
-  # models are identical across plots -> get once
-  model_names <- names(mult_plots_info[[1]])
-  n_models <- length(model_names)
-  cols <- grDevices::rainbow(n_models)
-
-  # layout matrix (+1 for legend)
-  layout_mat <- matrix(seq_len(nrow * ncol), nrow, ncol, byrow = TRUE)
-  layout_mat <- rbind(layout_mat, rep(nrow * ncol + 1, ncol))
-
-  if (dev.cur() == 1) {
-  dev.new()
-  } else {
-  graphics.off()
-  dev.new()
-  }
-
-  layout(layout_mat, heights = c(rep(1, nrow), 0.25))
-
-  old_par <- par(no.readonly = TRUE)
-  # on.exit(par(old_par))
-
-  par(mar = c(4, 4, 3, 1) + 0.1)
-
-  # draw plots
-  for (outer_name in outer_names) {
-
-    inner_list <- mult_plots_info[[outer_name]]
-
-    ylim <- range(
-      unlist(lapply(inner_list, function(z) z[[1]])),
-      na.rm = TRUE
-    )
-
-    first <- inner_list[[1]]
-    plot(
-      first[[2]], first[[1]],
-      type = "b",
-      col = cols[1],
-      lwd = lwd,
-      pch = point_pch,
-      ylim = ylim,
-      xlab = xlab,
-      ylab = ylab,
-      main = outer_name
-    )
-
-    if (length(inner_list) > 1) {
-      for (i in 2:length(inner_list)) {
-        lines(
-          inner_list[[i]][[2]],
-          inner_list[[i]][[1]],
-          type = "b",
-          col = cols[i],
-          lwd = lwd,
-          pch = point_pch
-        )
-      }
-    }
-  }
-
-  # legend panel
-  par(mar = c(0, 0, 0, 0))
-  plot.new()
-  legend(
-    "center",
-    legend = model_names,
-    col = cols,
-    lwd = lwd,
-    pch = point_pch,
-    ncol = min(length(model_names), 4),
-    cex = legend_cex,
-    bty = "n"
-  )
-}
+# plot_multilevel <- function(
+#   mult_plots_info,
+#   ncol = NULL,                   # number of columns in layout
+#   xlab = "Number of observations",
+#   ylab = "Accuracy",
+#   lwd = 2,
+#   point_pch = 16,
+#   legend_cex = 0.7
+# ) {
+#   stopifnot(is.list(mult_plots_info))
+#
+#   outer_names <- names(mult_plots_info)
+#   n_plots <- length(outer_names)
+#
+#   # determine layout
+#   if (is.null(ncol)) {
+#     ncol <- ceiling(sqrt(n_plots))
+#   }
+#   nrow <- ceiling(n_plots / ncol)
+#
+#   # models are identical across plots -> get once
+#   model_names <- names(mult_plots_info[[1]])
+#   n_models <- length(model_names)
+#   cols <- grDevices::rainbow(n_models)
+#
+#   # layout matrix (+1 for legend)
+#   layout_mat <- matrix(seq_len(nrow * ncol), nrow, ncol, byrow = TRUE)
+#   layout_mat <- rbind(layout_mat, rep(nrow * ncol + 1, ncol))
+#
+#   if (dev.cur() == 1) {
+#   dev.new()
+#   } else {
+#   graphics.off()
+#   dev.new()
+#   }
+#
+#   layout(layout_mat, heights = c(rep(1, nrow), 0.25))
+#
+#   old_par <- par(no.readonly = TRUE)
+#   # on.exit(par(old_par))
+#
+#   par(mar = c(4, 4, 3, 1) + 0.1)
+#
+#   # draw plots
+#   for (outer_name in outer_names) {
+#
+#     inner_list <- mult_plots_info[[outer_name]]
+#
+#     ylim <- range(
+#       unlist(lapply(inner_list, function(z) z[[1]])),
+#       na.rm = TRUE
+#     )
+#
+#     first <- inner_list[[1]]
+#     plot(
+#       first[[2]], first[[1]],
+#       type = "b",
+#       col = cols[1],
+#       lwd = lwd,
+#       pch = point_pch,
+#       ylim = ylim,
+#       xlab = xlab,
+#       ylab = ylab,
+#       main = outer_name
+#     )
+#
+#     if (length(inner_list) > 1) {
+#       for (i in 2:length(inner_list)) {
+#         lines(
+#           inner_list[[i]][[2]],
+#           inner_list[[i]][[1]],
+#           type = "b",
+#           col = cols[i],
+#           lwd = lwd,
+#           pch = point_pch
+#         )
+#       }
+#     }
+#   }
+#
+#   # legend panel
+#   par(mar = c(0, 0, 0, 0))
+#   plot.new()
+#   legend(
+#     "center",
+#     legend = model_names,
+#     col = cols,
+#     lwd = lwd,
+#     pch = point_pch,
+#     ncol = min(length(model_names), 4),
+#     cex = legend_cex,
+#     bty = "n"
+#   )
+# }
 
 create_multilevel_plot <- function(
   mult_plots_info,
