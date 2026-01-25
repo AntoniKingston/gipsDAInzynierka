78b736efbb7933181a2e666293b1f8145542dbd7
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2026-01-25 13:46:27 +0100

    pvalue checker print add

--- manual_tests/models/p_value_checker.R
+++ manual_tests/models/p_value_checker.R
@@ -14,6 +14,21 @@ truncated_data_list <- lapply(data_list, function(data_obj) {
   })
 })
 
-
-
-
+# print scenarios with p-values below threshold
+for (i in seq_along(truncated_data_list)) {
+    cat("File:", files[i], "\n")
+    for (scenario_name in scenario_names) {
+        p_values <- truncated_data_list[[i]][[scenario_name]]
+        if (length(p_values) > 0) {
+            cat("  Scenario:", scenario_name, "\n")
+            print(p_values)
+        }
+    }
+    cat("\n")
+}
+# count p-values below threshold for each scenario and each file
+count_below_threshold <- lapply(truncated_data_list, function(data_obj) {
+    sapply(data_obj, length)
+})
+names(count_below_threshold) <- files
+print(count_below_threshold)
